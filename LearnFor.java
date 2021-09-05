d75126dad721da934744392d5bc44ca6ff59603e
Roseboscom <roseboscom@gmail.com>
2021-09-05 08:23:24 +0530

    Add files via upload

---
+++ LearnFor.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class LearnFor {
+
+	public static void main(String[] args) {
+		System.out.println("The even numbers between 1 and 50 are :");
+		for(int i=1;i<=50;i++)
+		{
+			if(i%2==0)
+				System.out.println(i);
+		}
+
+	}
+
+}
---
+++ LearnVariablesDataTypes.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class LearnVariablesDataTypes {
+
+	
+
+	public static void main(String[] args) {
+
+		byte ramSize = 8;
+		String color = "blue";
+		byte pixelSize = 14;
+		float screenSize = 5.5f;
+		int weight = 194;
+		System.out.println("RamSize   :" + ramSize);
+		System.out.println("Color     :" + color);
+		System.out.println("PixelSize :" + pixelSize);
+		System.out.println("Screensize:" + screenSize);
+		System.out.println("weight    :" + weight);
+	}
+
+}
